35fe8255ad0afaa702175f37f7346b51786db773
James Lin <jameslin101@gmail.com>
2024-07-13 06:46:19 -0400

    add 2022-23 salary

--- process_data.py
+++ process_data.py
@@ -1,5 +1,10 @@
+import pandas as pd
+
+# Load the NBA salaries data
+nba_salaries_df = pd.read_csv('./Nba Player Salaries.csv')
+
 # Filter the data for the 2022/2023 season
 salaries_2022_2023 = nba_salaries_df[['Player Name', '2022/2023']]
 
 # Save the filtered data to a new CSV file
-salaries_2022_2023.to_csv('/mnt/data/nba_salaries_2022_2023.csv', index=False)
+salaries_2022_2023.to_csv('./nba_salaries_2022_2023.csv', index=False)
